e89e6b3f6e2657f3cf7dd97be850ea4c9b979748
Sam Blackman <sblackman@gmail.com>
2015-02-18 17:25:43 -0500

    add not null constraint in database for email

--- db/migrate/20150218211645_create_users.rb
+++ db/migrate/20150218211645_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username, null: false
       t.string :password_digest, null: false
-      t.string :email
+      t.string :email, null: false
       t.string :first_name
       t.string :last_name
 
