c15a5b090032c7968a455f88615ad0e0c0253194
Sam Blackman <sblackman@gmail.com>
2015-02-18 17:40:43 -0500

    write test for happy path valid user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 describe User do
-  it "is valid with a username, password, firstname, lastname and email"
+  it "is valid with a username, password, firstname, lastname and email" do
+    user = User.new(username: "tsamb",
+      password: "password",
+      first_name: "Sam",
+      last_name: "Blackman",
+      email: "sam@example.com")
+    expect(user).to be_valid
+  end
+
   it "is invalid without a username"
   it "is invalid without a password"
   it "is invalid without an email address"
