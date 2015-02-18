94e3d4433349c4d5b43ff6772abfee540af6bff2
Sam Blackman <sblackman@gmail.com>
2015-02-18 17:35:03 -0500

    add pending user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 describe User do
-  it "passes for a tautology" do
-    expect(true).to eq(true)
-  end
+  it "is valid with a username, password, firstname, lastname and email"
+  it "is invalid without a username"
+  it "is invalid without a password"
+  it "is invalid without an email address"
+  it "is invalid with a duplicate email address"
+  it "is invalid with a duplicate username"
+  it "returns a contact's full name as a string"
 end
