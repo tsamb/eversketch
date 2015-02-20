890ac89c7a0e886f3a4980b73fd6a27ffaf7e5f1
Sam Blackman <sblackman@gmail.com>
2015-02-20 01:09:13 -0500

    User#name returns first name + last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
