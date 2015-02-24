9b4261f794c0559eaa8bc3310b86ea8cb83a94b5
Sam Blackman <sblackman@gmail.com>
2015-02-24 18:23:16 +1300

    fix duplicated attributes in sketch tests
    Accidentally included `user_id` twice in attributes for mock test data.
    
    Replaced `user_id` with the correct `parent_id`.

--- spec/models/sketch_spec.rb
+++ spec/models/sketch_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe Sketch do
   it "is valid with a tree_id, user_id, parent_id and json_data " do
-    sketch = Sketch.new(tree_id: 1, user_id: 1, user_id: 1, json_data: "{\"random\":\"json data\"}")
+    sketch = Sketch.new(tree_id: 1, user_id: 1, parent_id: 1, json_data: "{\"random\":\"json data\"}")
     expect(sketch).to be_valid
   end
 
   it "is invalid without a tree_id" do
-    sketch = Sketch.new(tree_id: nil, user_id: 1, user_id: 1, json_data: "{\"random\":\"json data\"}")
+    sketch = Sketch.new(tree_id: nil, user_id: 1, parent_id: 1, json_data: "{\"random\":\"json data\"}")
     expect(sketch).to_not be_valid
   end
 end
