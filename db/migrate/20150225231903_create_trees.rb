c0cf84828c24e0d7f2a463e3686f9ea2bb1c9f11
Sam Blackman <sblackman@gmail.com>
2015-02-26 12:25:39 +1300

    add and run create trees table migration

---
+++ db/migrate/20150225231903_create_trees.rb
@@ -0,0 +1,9 @@
+class CreateTrees < ActiveRecord::Migration
+  def change
+    create_table :trees do |t|
+      t.integer :origin_id
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150221161426) do
+ActiveRecord::Schema.define(version: 20150225231903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20150221161426) do
     t.integer  "user_id"
     t.integer  "tree_id"
     t.integer  "parent_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trees", force: :cascade do |t|
+    t.integer  "origin_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
